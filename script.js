40d6ba1fb7fadd60805e517855954ad8c6e1fa9e
Infant Reshmi Eugine <92745419+infantreshmi-e@users.noreply.github.com>
2024-11-30 01:07:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Smooth Scroll for Anchor Links
+    const links = document.querySelectorAll('a[href^="#"]');
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
